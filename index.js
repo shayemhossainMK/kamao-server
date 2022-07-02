20f068f273c9cb2ffeb08c8c1e7b3f869adc38f8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-07-02 17:58:01 +0600

    cors problem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // this is midlewiere
-app.use(cors());
+app.use(cors({ origin: `https://mk-social-commerce.web.app/` }));
 app.use(express.json());
 
 // This is from database
