292f5f48ff74f7bc336edc72bfbb8e263412e474
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-07-02 18:45:20 +0600

    enw oa

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // this is midlewiere
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mk-social-commerce.web.app/",
+  })
+);
 app.use(express.json());
 
 // This is from database
