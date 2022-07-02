30ab0bbf1ea4ae7f055c99733ce1857b29071092
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-07-02 18:31:34 +0600

    new dode

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // this is midlewiere
-app.use(
-  cors({
-    origin: "https://mk-social-commerce.web.app/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // This is from database
